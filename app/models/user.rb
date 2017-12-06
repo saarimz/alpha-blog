a5d9c87deb124501b907fdbdf5db590ad5eca28b
Saarim Zaman <saarim.z@gmail.com>
2017-12-06 02:12:30 +0000

    Signup users from UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,6 @@ class User < ActiveRecord::Base
             uniqueness: { case_sensitive: false }, 
             format: { with: VALID_EMAIL_REGEX }
             
-  
   has_secure_password
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
   
+  get "signup" => "users#new"
+  resources :users, except: [:new]
+  
   resources :articles
   
 

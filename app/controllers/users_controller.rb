e5aa0d1afa0629a83154670b2d62d855d9eff5e6
Saarim Zaman <saarim.z@gmail.com>
2018-02-04 20:14:49 +0000

    Add categories table model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
   before_action :require_same_user, only: [:edit, :update, :destroy]
   before_action :require_admin, only: [:destroy]
+  
   def new
     @user = User.new
   end

e409cd7067bc1da4759463c8911fe861acf11e8e
Saarim Zaman <saarim.z@gmail.com>
2018-02-04 21:39:22 +0000

    added integration tests

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,31 @@
+class CategoriesController < ApplicationController
+  
+  def index
+    @categories = Category.paginate(page: params[:page], per_page: 5)
+  end
+  
+  def show
+    
+  end
+  
+  def new
+    @category = Category.new
+  end
+  
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "You have created a new category successfully"
+      redirect_to categories_path
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def category_params
+    params.require(:category).permit(:name)
+  end
+  
+end
